9938e0adc0cf6ba98b48a896353111c99d5d3d52
redmejia <reynaldoemejia@gmail.com>
2025-05-08 11:48:18 -0700

    message bubble and test data

--- app/src/main/java/com/bitinovus/verbapp/presentation/screens/chat/Chat.kt
+++ app/src/main/java/com/bitinovus/verbapp/presentation/screens/chat/Chat.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,7 +29,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.bitinovus.verbapp.presentation.components.bubble.Bubble
 import com.bitinovus.verbapp.presentation.components.prompt.PromptTextField
 import com.bitinovus.verbapp.presentation.ui.theme.PrimaryGray00
 import com.bitinovus.verbapp.presentation.ui.theme.PrimaryBackground
@@ -40,6 +45,31 @@ fun Chat(
 ) {
     var textFieldValue by remember { mutableStateOf("") }
 
+    val messageList = listOf<Messages>(
+        Messages(
+            message = "this ", id = 1
+        ),
+        Messages(message = "Dos", id = 2),
+        Messages(message = "meeeeee", id = 3),
+        Messages(message = "Uno", id = 4),
+        Messages(message = "Udasdsadno", id = 5),
+        Messages(message = "Uno", id = 6),
+        Messages(
+            message = "this is test of long text, " +
+                    "this is test of long text,  this is test of long" +
+                    " text this is test of long text,  this is test of long text ",
+            id = 7
+        ),
+        Messages(message = "Udasdasno", id = 8),
+        Messages(message = "Uno", id = 9),
+        Messages(message = "Undasdaso", id = 10),
+        Messages(message = "Undasdsao", id = 11),
+        Messages(message = "Uno", id = 12),
+        Messages(message = "Udasdsano", id = 13),
+        Messages(message = "Uewqeqwno", id = 14),
+        Messages(message = "Undasdaso", id = 15),
+    )
+
     Box(
         modifier = Modifier
             .background(color = PrimaryBackground)
@@ -48,6 +78,7 @@ fun Chat(
             .statusBarsPadding()
             .fillMaxSize()
     ) {
+
         LazyColumn(
             modifier = Modifier
                 .windowInsetsPadding(WindowInsets.systemBars)
@@ -60,13 +91,19 @@ fun Chat(
                 // reverse space between PromptTextField and Bubble Text
                 Spacer(modifier = Modifier.height(10.dp))
             }
-            items(count = 30) {
-                Text(
-                    modifier = Modifier
-                        .background(color = if(it % 2 == 0) PrimaryGray00 else senderBubble)
-                        .padding(all = 16.dp)
-                        .fillMaxWidth(),
-                    text = "Item $it"
+
+            items(items = messageList, key = { it.id }) {
+                val isSender = it.id % 2 != 0
+                Bubble(
+                    backgroundColor = if (isSender) senderBubble else PrimaryGray00,
+                    isSender = isSender,
+                    contentText = {
+                        Text(
+                            text = it.message,
+                            color = if (isSender) Color.White else Color.Black,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
                 )
             }
         }
@@ -88,3 +125,9 @@ fun Chat(
         }
     }
 }
+
+
+data class Messages(
+    val message: String,
+    val id: Int,
+)
